eb70686902eaf8dc4f8c16fb523832e2bafc6e59
p-racoon <prasham.ashesh>
2019-04-07 01:00:55 +0530

    Bugfix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import About from './About';
-import ReactFormBootstrap from './About';
+import ReactFormBootstrap from './ReactFormBootstrap';
 
 const Main = () => {
     return (
